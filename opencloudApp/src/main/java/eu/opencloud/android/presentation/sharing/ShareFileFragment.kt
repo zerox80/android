adc948457fc8919751da7b73abe4d72651d55da6
zerox80 <rujbin@proton.me>
2025-11-26 18:30:41 +0100

    Fix Detekt issues

--- opencloudApp/src/main/java/eu/opencloud/android/presentation/sharing/ShareFileFragment.kt
+++ opencloudApp/src/main/java/eu/opencloud/android/presentation/sharing/ShareFileFragment.kt
@@ -240,13 +240,15 @@ class ShareFileFragment : Fragment(), ShareUserListAdapter.ShareUserAdapterListe
             )
         )
         if (file!!.isImage) {
-            binding.shareFileIcon.load(ThumbnailsRequester.getPreviewUriForFile(file!!, account!!), ThumbnailsRequester.getCoilImageLoader(account!!)) {
+            binding.shareFileIcon.load(
+                ThumbnailsRequester.getPreviewUriForFile(file!!, account!!),
+                ThumbnailsRequester.getCoilImageLoader(account!!)
+            ) {
                 placeholder(MimetypeIconUtil.getFileTypeIconId(file!!.mimeType, file!!.fileName))
                 error(MimetypeIconUtil.getFileTypeIconId(file!!.mimeType, file!!.fileName))
                 crossfade(true)
             }
         }
-        // Name
         binding.shareFileName.text = file?.fileName
 
         // Size
