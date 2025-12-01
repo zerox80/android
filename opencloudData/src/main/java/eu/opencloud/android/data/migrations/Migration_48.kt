ab33fd6f273121abd5a59f42dace243dda03bfd1
zerox80 <rujbin@proton.me>
2025-12-01 12:35:47 +0100

    Fix migration 48 crash by adding missing transfer columns

--- opencloudData/src/main/java/eu/opencloud/android/data/migrations/Migration_48.kt
+++ opencloudData/src/main/java/eu/opencloud/android/data/migrations/Migration_48.kt
@@ -26,5 +26,13 @@ val MIGRATION_47_48 = object : Migration(47, 48) {
             "ALTER TABLE ${ProviderTableMeta.CAPABILITIES_TABLE_NAME} " +
                 "ADD COLUMN `${ProviderTableMeta.CAPABILITIES_TUS_SUPPORT_HTTP_METHOD_OVERRIDE}` TEXT"
         )
+
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusUploadUrl` TEXT")
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusUploadLength` INTEGER")
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusUploadMetadata` TEXT")
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusUploadChecksum` TEXT")
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusResumableVersion` TEXT")
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusUploadExpires` INTEGER")
+        database.execSQL("ALTER TABLE ${ProviderTableMeta.TRANSFERS_TABLE_NAME} ADD COLUMN `tusUploadConcat` TEXT")
     }
 }
