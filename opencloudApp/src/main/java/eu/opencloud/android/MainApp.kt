aa10ad60c1f60d479636808f155e9e1c48bad8d1
zerox80 <rujbin@proton.me>
2026-01-19 13:24:49 +0100

    Refactor thumbnail caching: optimize sizes, fix thread safety, remove dead code

--- opencloudApp/src/main/java/eu/opencloud/android/MainApp.kt
+++ opencloudApp/src/main/java/eu/opencloud/android/MainApp.kt
@@ -40,7 +40,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.content.pm.PackageInfoCompat
 import eu.opencloud.android.data.providers.implementation.OCSharedPreferencesProvider
 
-import eu.opencloud.android.datamodel.ThumbnailsCacheManager
+
 import eu.opencloud.android.db.PreferenceManager
 import eu.opencloud.android.dependecyinjection.commonModule
 import eu.opencloud.android.dependecyinjection.localDataSourceModule
@@ -117,8 +117,7 @@ class MainApp : Application() {
 
         SingleSessionManager.setUserAgent(userAgent)
 
-        // initialise thumbnails cache on background thread
-        ThumbnailsCacheManager.InitDiskCacheTask().execute()
+
 
         initDependencyInjection()
 
